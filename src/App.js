564e05e096d8e9d1e39e3ec6305d014fb21339f3
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2023-11-28 11:45:19 +0530

    App modified

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         return <div className="microphone-container">Browser does not support Speech Recognition.</div>;
     }
 
-    const handleListing = () => {
+    const handleListening = () => {
         setIsListening(true);
         microphoneRef.current.classList.add("listening");
         SpeechRecognition.startListening({
@@ -25,6 +25,7 @@ function App() {
         microphoneRef.current.classList.remove("listening");
         SpeechRecognition.stopListening();
         console.log(transcript);
+        resetTranscript();
     };
 
     const handleReset = () => {
@@ -33,29 +34,55 @@ function App() {
     };
 
     return (
-        <div className="microphone-wrapper">
-            <div className="mircophone-container">
-                <div className="microphone-icon-container" ref={microphoneRef} onClick={handleListing}>
-                    <img src={microPhoneIcon} className="microphone-icon" alt="Microphone" />
-                </div>
-                <div className="microphone-status">
-                    {isListening ? "Listening........." : "Click to start Listening"}
-                </div>
-                {isListening && (
-                    <button className="microphone-stop btn" onClick={stopHandle}>
-                        Stop
-                    </button>
-                )}
+        <div className="App">
+            <div className="HeaderContainer">
+                <h1 className="Header">Speech Assist</h1>
             </div>
-            {transcript && (
-                <div className="microphone-result-container">
-                    <div className="microphone-result-text">{transcript}</div>
-                    <button className="microphone-reset btn" onClick={handleReset}>
-                        Reset
+            <div className="MicrophoneContainer">
+                <div className="MicrophoneIconContainer">
+                    <button className="StartButton" ref={microphoneRef} onClick={handleListening}>
+                        <img src={microPhoneIcon} className="MicrophoneIcon" alt="Microphone" />
                     </button>
                 </div>
-            )}
+                <div className="MicrophoneStatus">
+                    {isListening ? (
+                        <h2 className="MicrophoneStatusText">Listening...</h2>
+                    ) : (
+                        <h2 className="MicrophoneStatusText">Click and Start Speaking</h2>
+                    )}
+                </div>
+                <div className="StopButtonContainer">
+                    {isListening && (
+                        <button className="StopButton" onClick={stopHandle}>
+                            Stop
+                        </button>
+                    )}
+                </div>
+            </div>
         </div>
+        //     <div className="microphone-wrapper">
+        //         <div className="mircophone-container">
+        //             <div className="microphone-icon-container" ref={microphoneRef} onClick={handleListing}>
+        //                 <img src={microPhoneIcon} className="microphone-icon" alt="Microphone" />
+        //             </div>
+        //             <div className="microphone-status">
+        //                 {isListening ? "Listening........." : "Click to start Listening"}
+        //             </div>
+        //             {isListening && (
+        //                 <button className="microphone-stop btn" onClick={stopHandle}>
+        //                     Stop
+        //                 </button>
+        //             )}
+        //         </div>
+        //         {transcript && (
+        //             <div className="microphone-result-container">
+        //                 <div className="microphone-result-text">{transcript}</div>
+        //                 <button className="microphone-reset btn" onClick={handleReset}>
+        //                     Reset
+        //                 </button>
+        //             </div>
+        //         )}
+        //     </div>
     );
 }
 
